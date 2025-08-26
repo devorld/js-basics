cf8170b4632ee26348d5bbfc8203fb9e2e126cff
devorld <jsdevlop@gmail.com>
2025-08-27 01:23:52 +0400

    Monolog, Graphemes - improved calculations and added headers visualization

--- utils/graphemes.js
+++ utils/graphemes.js
@@ -1,7 +1,12 @@
-function getGraphemeCount(str) {
-    const segmenter = new Intl.Segmenter("en-US", { granularity: "grapheme" });
+const ID_SPACE = '\u3000';
 
-    return [...segmenter.segment(str)].length;
+function getGraphemeCount(str) {
+    const segmenter = new Intl.Segmenter("en-US", {granularity: "grapheme"});
+
+    // from \x1b[0m to \x1b[100m
+    const ansiColorRegEx = /\x1b\[\d+m/gi;
+
+    return [...segmenter.segment(str.replaceAll(ansiColorRegEx, ''))].length;
 }
 
-export { getGraphemeCount };
+export {ID_SPACE, getGraphemeCount};
