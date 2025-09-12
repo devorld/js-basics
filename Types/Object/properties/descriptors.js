3e432f7a5d8a15603b9cb3fb75e6c6f8b9b40627
devorld <jsdevlop@gmail.com>
2025-09-12 15:27:23 +0400

    remapDescriptors - refactored via switch...case logic

--- Types/Object/properties/descriptors.js
+++ Types/Object/properties/descriptors.js
@@ -13,35 +13,10 @@ const remapDescriptors = (obj) => Object.fromEntries(
                     if (d?.value !== null) {
                         return [`${k} ?${vType}?>`, remapDescriptors(d?.value)];
                     }
-                // null -> default:
-                default:
-                    // other type values
-                    return [`${k} ?${vType}?>`, d?.value];
             }
-
-            /*
-            * commit and delete
-            * correctly works before re-writing to switch()...case:
-            return d?.writable !== undefined && !k.endsWith("?>")
-                ? typeof d?.value === "object" && d?.value !== null
-                    // object value, not null
-                    ? [`${k} ?${typeof d?.value}?>`, remapDescriptors(d?.value)]
-                    // undefined, function, other values
-                    : ["undefined", "function"].includes(typeof (d?.value))
-                        // undefined, function values
-                        ? typeof d?.value === "function"
-                            // function type value
-                            ? [`${k} ?${typeof d?.value}?>`, funcBodyReplacer(d?.value)]
-                            // undefined type value
-                            : [`${k} ?${typeof d?.value}?>`, String(d?.value)]
-                        // other type values
-                        : [`${k} ?${typeof d?.value}?>`, d?.value]
-                // getter and setter
-                : [k, Object.assign(Object.create(null), {
-                    get: funcBodyReplacer(d?.get),
-                    set: funcBodyReplacer(d?.set)
-                })];
-             */
+            // null -> default:
+            // other type values
+            return [`${k} ?${vType}?>`, d?.value];
         }
     )
 );
