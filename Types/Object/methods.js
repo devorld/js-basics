3f1c2cc17046be4e502de846fd37ef269e98cd1c
devorld <jsdevlop@gmail.com>
2025-09-13 22:34:41 +0400

    Object - improved data getting methods examples

--- Types/Object/methods.js
+++ Types/Object/methods.js
@@ -1,53 +1,59 @@
-import {CONSOLE_TEXT_COLOR} from '../../utils/console.js'
-import {monolog} from '../../utils/monolog.js'
+import {CTC} from '../../utils/console.js'
+import {printer} from '../../utils/monolog.js'
 import {objSymExample} from './create.js'
 
-// noinspection JSUnresolvedReference - analogue of __name__ == "__main__"
-const isMainModule = import.meta.main;
+const clr = new Proxy(CTC ?? {}, {get: (t, p) => t?.[p] || ""})
+const p = new Proxy(printer ?? {}, {get: (t, f) => t?.[f] || console.log})
 
-const p = {
-    head: monolog?.printHeader || console.log,
-    title: monolog?.printSubHeader || console.log,
-    buff: monolog?.pushStringParts?.bind(monolog) || console.log,
-    flush: monolog?.printLines?.bind(monolog) || console.log,
-    cNo: CONSOLE_TEXT_COLOR?.Reset || '',
-    cProp: CONSOLE_TEXT_COLOR?.FgMagenta || '',
-    cVar: CONSOLE_TEXT_COLOR?.FgBlue || '',
-};
+const objSymChild = Object.defineProperties(Object.create(objSymExample), Object.getOwnPropertyDescriptors(objSymExample));
 
-if (isMainModule) {
-    p.buff(`${p.cVar}objExample${p.cNo}`, '█►', objSymExample);
-    p.flush();
+Object.defineProperty(objSymChild, "5", {enumerable: false});
+Object.defineProperty(objSymChild, Object.getOwnPropertySymbols(objSymChild)[0], {enumerable: false});
 
-    // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ (for .. in) -- objSymExample and [[Prototype]]es ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-    p.head("(for .. in) -- objSymExample and [[Prototype]]es")
+// ░░░░░░░░░░░░░░░░░░░░░░░░ console.dir(objSymChild) - ❌  no enumerable prop "5" ░░░░░░░░░░░░░░░░░░░░░░░░
+p.title("console.dir(objSymChild) - ❌  no enumerable prop \"5\"");
+console.dir(objSymChild);
 
-    const props = Object.create(null);
+// ░░░░░░░░░░░░░░░░░ console.dir(Object.getOwnPropertyDescriptors(objSymChild)) - ✅ All ░░░░░░░░░░░░░░░░░
+p.title("console.dir(Object.getOwnPropertyDescriptors(objSymChild)) - ✅  All");
+console.dir(Object.getOwnPropertyDescriptors(objSymChild));
 
-    for (let pName in objSymExample) {
-        props[pName] = objSymExample.hasOwnProperty(pName) ? "self" : "inherited";
-    }
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ objSymChild ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+p.title(`${clr.var}objSymChild${clr.reset}`);
+p.buff(`JSON${clr.func}.stringify${clr.reset}(objExample)`, clr.sep, JSON.stringify(objSymChild));
+p.buff(`${clr.var}objSymChild${clr.reset}`, `\n${clr.sep}`, objSymChild);
 
-    p.buff(`${p.cProp}props${p.cNo}`, `\n█►`, props);
 
-    p.head("without Symbolic keys");
-    p.title(`${p.cProp}.keys() .values() .entries()${p.cNo} - only {enumerable: true}, ❌ no [[Prototype]] properties`);
-    p.buff(`JSON${p.cProp}.stringify${p.cNo}(objExample)`, '█►', JSON.stringify(objSymExample));
-        p.buff(`Object${p.cProp}.keys${p.cNo}(objExample)`, '█►', Object.keys(objSymExample));
-    p.buff(`Object${p.cProp}.values${p.cNo}(objExample)`, '█►', Object.values(objSymExample));
-    p.buff(`Object${p.cProp}.entries${p.cNo}(objExample)`, '█►', Object.entries(objSymExample));
-    p.buff(`Object${p.cProp}.getOwnPropertyNames${p.cNo}(objExample)`, '█►', Object.getOwnPropertyNames(objSymExample));
-    p.buff(`Object${p.cProp}.getOwnPropertyDescriptors${p.cNo}(objExample)`, '█►', Object.getOwnPropertyDescriptors(objSymExample));
-    p.flush();
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ All, non-symbol, symbol properties: keys and values ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+p.head("All, non-symbol, symbol properties: keys and values");
 
-    p.head("only Symbolic keys");
-    p.buff(`Object${p.cProp}.getOwnPropertySymbols${p.cNo}(objExample)`, '█►', Object.getOwnPropertySymbols(objSymExample));
-    p.flush();
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ All = 8 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+p.title("All = 8");
+p.buff(`${clr.func}Reflect.ownKeys${clr.reset}(objSymChild)`, clr.sep, Reflect.ownKeys(objSymChild));
+p.flush({keepMaxLengths: true})
 
-    p.head("with Symbolic keys");
-    p.buff(`Reflect${p.cProp}.ownKeys${p.cNo}(objExample)`, '█►', Reflect.ownKeys(objSymExample));
-    p.buff();
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ non-symbol all keys = 6 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+p.title("non-symbol all keys = 6");
+p.buff(`Object${clr.func}.getOwnPropertyNames${clr.reset}(objSymChild)`, clr.sep, Object.getOwnPropertyNames(objSymChild));
+p.flush({keepMaxLengths: true})
 
-    p.buff(`${p.cVar}objExample${p.cNo}`, '█►', objSymExample);
-    p.flush();
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ non-symbol only enumerable keys = 5 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+p.title("non-symbol only enumerable keys = 5");
+p.buff(`Object${clr.func}.keys${clr.reset}(objSymChild)`, clr.sep, Object.keys(objSymChild));
+
+const keysForIn = [];
+
+for (let key in objSymChild) {
+    keysForIn.push(key);
 }
+
+p.buff(`(${clr.func}for .. in ${clr.var}objSymChild${clr.reset})`, clr.sep, keysForIn);
+
+p.buff(`Object${clr.func}.values${clr.reset}(objSymChild)`, clr.sep, Object.values(objSymChild));
+p.buff(`Object${clr.func}.entries${clr.reset}(objSymChild)`, clr.sep, Object.entries(objSymChild));
+p.flush({keepMaxLengths: true})
+
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ symbol all keys = 2 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+p.title("symbol all keys = 2");
+p.buff(`Object${clr.func}.getOwnPropertySymbols${clr.reset}(objSymChild)`, clr.sep, Object.getOwnPropertySymbols(objSymChild));
+p.flush();
