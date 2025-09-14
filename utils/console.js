fa6dff4fcd566aedcc6d26f376531c1e0475cf36
devorld <jsdevlop@gmail.com>
2025-09-15 00:20:00 +0400

    Object - added extended examples in _examples.js

--- utils/console.js
+++ utils/console.js
@@ -1,3 +1,6 @@
+import {objSymExample} from '../Types/Object/_examples.js'
+import {printer} from './monolog.js'
+
 const CONSOLE_TEXT_COLOR = {
     Reset: '\x1b[0m',
     Bright: '\x1b[1m',
@@ -28,36 +31,65 @@ const CONSOLE_TEXT_COLOR = {
     BgGray: '\x1b[100m',
 };
 
+// noinspection SpellCheckingInspection
 const CTC = {
     reset: CONSOLE_TEXT_COLOR.Reset,
     func: CONSOLE_TEXT_COLOR.FgMagenta,
     var: CONSOLE_TEXT_COLOR.FgBlue,
     sep: '█►',
+    nsep: "\n█►",
 };
 
-debug: {
-    break debug;
+const clr = CTC;
+const p = new Proxy(printer ?? {}, {get: (t, f) => t?.[f] || console.log});
 
-    // disabled till debugging
-    // noinspection UnreachableCodeJS,JSUnusedAssignment
-    const CTC = CONSOLE_TEXT_COLOR;
+function printAll(obj, headText = `${clr.func}console.${clr.reset}$display$()`) {
+    p.head(headText);
 
-    console.log(`${CTC.FgBlue}Lorem ipsum${CTC.Reset}`);
+    p.title(`console${clr.func}.table${clr.reset}(obj)`);
+    console.table(obj);
 
-    // region Group - Object under the hood
-    console.groupCollapsed('Group - Object under the hood');
+    p.title(`console${clr.func}.log${clr.reset}(obj)`);
+    console.log(obj);
 
-    console.dir(Object, {showHidden: true, colors: true, depth: null});
+    p.title(`console${clr.func}.dir${clr.reset}(Object.getOwnPropertyDescriptors(obj))`);
+    console.dir(obj);
 
-    console.groupEnd();
-    // endregion
+    p.title(`console${clr.func}.dir${clr.reset}(obj, {${clr.func}showHidden: true${clr.reset}})`);
+    console.dir(obj, {showHidden: true});
 
-    console.table(Object.getOwnPropertyDescriptors(Object));
-
-    console.assert(CTC.hasOwnProperty('BgGray'), 'Color %s not found!', 'gray');
-    console.assert(CTC.hasOwnProperty('BgGrey'), `${CTC.BgRed}Color %s not found!${CTC.Reset}`, 'grey');
-
-    console.trace('Lorem', 'ipsum', 0);
+    p.title(`console${clr.func}.dir${clr.reset}(Object.getOwnPropertyDescriptors(obj), {${clr.func}showHidden: true${clr.reset}})`);
+    console.dir(Object.getOwnPropertyDescriptors(obj), {showHidden: true});
 }
 
-export { CONSOLE_TEXT_COLOR, CTC };
+// noinspection JSUnresolvedReference - analogue of __name__ == "__main__"
+const isMainModule = import.meta.main;
+
+if (isMainModule) {
+    printAll(objSymExample);
+
+    debug: {
+        break debug;
+
+        // disabled till debugging
+        // noinspection UnreachableCodeJS
+        console.log(`${CONSOLE_TEXT_COLOR.FgBlue}Lorem ipsum${CONSOLE_TEXT_COLOR.Reset}`);
+
+        // region Group - Object under the hood
+        console.groupCollapsed('Group - Object under the hood');
+
+        console.dir(Object, {showHidden: true, colors: true, depth: null});
+
+        console.groupEnd();
+        // endregion
+
+        console.table(Object.getOwnPropertyDescriptors(Object));
+
+        console.assert(CONSOLE_TEXT_COLOR.hasOwnProperty('BgGray'), 'Color %s not found!', 'gray');
+        console.assert(CONSOLE_TEXT_COLOR.hasOwnProperty('BgGrey'), `${CONSOLE_TEXT_COLOR.BgRed}Color %s not found!${CONSOLE_TEXT_COLOR.Reset}`, 'grey');
+
+        console.trace('Lorem', 'ipsum', 0);
+    }
+}
+
+export {CONSOLE_TEXT_COLOR, CTC, printAll};
